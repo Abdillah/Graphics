d6263b8ce0cd5e67321ff9d4bab7c1e068e52c38
Hernawan Fa'iz Abdillah <abdillah96.bu@gmail.com>
2015-04-27 07:36:02 +0700

    Add render engine class

---
+++ Transform3D/CubeDrawingEngine.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transform3D
+{
+    class CubeDrawingEngine : Drawing2D
+    {
+		/**
+		 * @param canvas Any window component to draw on.
+		 */
+        public CubeDrawingEngine(Control canvas)
+		{
+			this.canvas = canvas;
+        }
+
+		private void DrawFace(Point3D[] face)
+		{
+			Point[] projection;
+
+			int i = 0;
+			foreach (Point3D p in face)
+			{
+				projection[i] = Project(p);
+				i++;
+			}
+
+			DrawLine(projection[0], projection[1]);
+			DrawLine(projection[1], projection[2]);
+			DrawLine(projection[2], projection[3]);
+			DrawLine(projection[3], projection[1]);
+		}
+
+		public void DrawCube(Point3D[] cube)
+		{
+			Point[] projection;
+
+			int i = 0;
+			foreach (Point3D p in cube)
+			{
+				projection[i] = Project(p);
+				i++;
+			}
+
+			// Draw line 1-2, 2-3, 3-4, 4-1
+			DrawFace(cube.GetFace(1));
+			// Draw line 5-6, 6-7, 7-8, 8-5
+			DrawFace(cube.GetFace(6));
+			// Draw line 1-5, 2-6, 3-7, 4-8
+			DrawLine(projection[1], projection[5]);
+			DrawLine(projection[2], projection[6]);
+			DrawLine(projection[3], projection[7]);
+			DrawLine(projection[4], projection[8]);
+		}
+
+    }
+}
---
+++ Transform3D/Drawing2D.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transform3D
+{
+    class Drawing2D
+    {
+		Control canvas;
+		PaintEventArgs e;
+
+		PaintEventArgs PaintEvent {
+			set {
+				this.e = value;
+			}
+			get {
+				return this.e;
+			}
+		};
+
+		/**
+		 * @brief Projecting 3D coordinate into 2D which involve
+		 *        recalculation of X and Y with the help of Z.
+		 * @param p point to project.
+		 * @return 2D Point which ready to be drawn or render.
+		 */
+        private Point Project(Point3D p)
+        {
+            Point normal = Normalize(new Point(p.X, p.Y));
+
+            int X_TRANSLATE = -3;
+            int Y_TRANSLATE = 4;
+
+            int x = normal.X;
+            int y = normal.Y;
+
+            Console.WriteLine("Normal : " + x + ", " + y);
+
+            if (p.Z == 0)
+            {
+                return new Point(x, y);
+            }
+            else
+            {
+                x = x + (p.Z * X_TRANSLATE);
+                y = y + (p.Z * Y_TRANSLATE);
+
+                Console.WriteLine("Project : " + x + ", " + y);
+                return new Point(x, y);
+            }
+        }
+
+		/**
+		 * @brief Translate point into bottom-left (0,0) and
+		 *        scale appropriately to enhance visibility.
+		 *        Used mainly to draw axis lines.
+		 * @param p raw point to normalize
+		 */
+        private Point NormalizeRaw(Point p)
+        {
+            int x = p.X * 5;
+            int y = ((p.Y * -1) * 5 + HEIGHT);
+
+            return new Point(x, y);
+        }
+
+		/**
+		 * @brief Translate point into pivot on (30,40) position
+		 *        relative to bottom-left and also scale appropriately
+		 *        to enhance visibility
+		 * @param p raw point to normalize
+		 */
+        private Point Normalize(Point p)
+        {
+            int x = (p.X + 30) * 5;
+            int y = (((p.Y + 40) * -1) * 5 + HEIGHT);
+
+            return new Point(x, y);
+        }
+
+		/**
+		 * @brief Wrap System.Graphics.DrawLine function, automatically
+		 *        project the points.
+		 */
+		public void DrawLine(Pen pen, Point3D p1, Point3D p2) {
+            e.Graphics.DrawLine(pen, Project(p1), Project(p2));
+		}
+
+		/**
+		 * @param canvas Any window component to draw on.
+		 */
+        public Drawing2D(Control canvas) {
+			this.canvas = canvas;
+        }
+
+    }
+}
