4e60e98a316a8ec06b5848f9f2b8a0c8829409d2
Hernawan Fa'iz Abdillah <abdillah96.bu@gmail.com>
2015-04-27 07:36:45 +0700

    Use newly created render engine in UI class

--- Transform3D/Form1.cs
+++ Transform3D/Form1.cs
@@ -14,58 +14,23 @@ namespace Transform3D
     {
         Pen axisPen;
         Pen pen;
+		Drawing2D engine;
+		CubeDrawingEngine cubeEngine;
+
         int WIDTH = 640;
         int HEIGHT = 480;
 
-        public Form1() 
+        public Form1()
         {
             InitializeComponent();
 
             pen = new Pen(Brushes.Red);
             pen.Width = 3;
+
             axisPen = new Pen(Brushes.Blue);
-        }
 
-        private Point project(Point3D p)
-        {
-            Point normal = normalize(new Point(p.X, p.Y));
-
-            int X_TRANSLATE = -3;
-            int Y_TRANSLATE = 4;
-
-            int x = normal.X;
-            int y = normal.Y;
-
-            Console.WriteLine("Normal : " + x + ", " + y);
-
-            if (p.Z == 0)
-            {
-                return new Point(x, y);
-            }
-            else
-            {
-                x = x + (p.Z * X_TRANSLATE);
-                y = y + (p.Z * Y_TRANSLATE);
-
-                Console.WriteLine("Project : " + x + ", " + y);
-                return new Point(x, y);
-            }
-        }
-
-        private Point normalizeRaw(Point p)
-        {
-            int x = p.X * 5;
-            int y = ((p.Y * -1) * 5 + HEIGHT);
-
-            return new Point(x, y);
-        }
-
-        private Point normalize(Point p)
-        {
-            int x = (p.X + 30) * 5;
-            int y = (((p.Y + 40) * -1) * 5 + HEIGHT);
-
-            return new Point(x, y);
+			engine = new Drawing2D(this.canvas);
+			cubeEngine = new CubeDrawingEngine(this.canvas);
         }
 
 		/**
@@ -80,45 +45,38 @@ namespace Transform3D
             p0 = new Point(0, 0);
             p1 = new Point(30, 40);
 
-            e.Graphics.DrawLine(axisPen, normalizeRaw(p0), normalizeRaw(p1));
+            e.Graphics.DrawLine(axisPen, NormalizeRaw(p0), NormalizeRaw(p1));
 
             p0 = new Point(30, 40);
             p1 = new Point(1000, 40);
 
-            e.Graphics.DrawLine(axisPen, normalizeRaw(p0), normalizeRaw(p1));
+            e.Graphics.DrawLine(axisPen, NormalizeRaw(p0), NormalizeRaw(p1));
 
             p0 = new Point(30, 40);
             p1 = new Point(30, 1000);
 
-            e.Graphics.DrawLine(axisPen, normalizeRaw(p0), normalizeRaw(p1));
+            e.Graphics.DrawLine(axisPen, NormalizeRaw(p0), NormalizeRaw(p1));
         }
 
         private void canvas_Paint(object sender, PaintEventArgs e)
         {
-            drawAxis(e);
+			engine.PaintEvent = e;
+			cubeEngine.PaintEvent = e;
+			
+            DrawAxis(e);
 
-            Point3D p3d0 = new Point3D(0, 0, 0);
-            Point3D p3d1 = new Point3D(0, 0, 30);
-			Point3D p3d2 = new Point3D(30, 0, 30);
-			Point3D p3d3 = new Point3D(30, 0, 0);
-			Point3D p3d4 = new Point3D(0, 30, 0);
-			Point3D p3d5 = new Point3D(0, 30, 30);
-			Point3D p3d6 = new Point3D(30, 30, 30);
-			Point3D p3d7 = new Point3D(30, 30, 0);
-			
-            e.Graphics.DrawLine(pen, project(p3d0), project(p3d1));
-			e.Graphics.DrawLine(pen, project(p3d1), project(p3d2));
-			e.Graphics.DrawLine(pen, project(p3d2), project(p3d3));
-			e.Graphics.DrawLine(pen, project(p3d3), project(p3d0));
-			e.Graphics.DrawLine(pen, project(p3d4), project(p3d5));
-			e.Graphics.DrawLine(pen, project(p3d5), project(p3d6));
-			e.Graphics.DrawLine(pen, project(p3d6), project(p3d7));
-			e.Graphics.DrawLine(pen, project(p3d7), project(p3d4));
-			
-			e.Graphics.DrawLine(pen, project(p3d0), project(p3d4));
-			e.Graphics.DrawLine(pen, project(p3d1), project(p3d5));
-			e.Graphics.DrawLine(pen, project(p3d2), project(p3d6));
-			e.Graphics.DrawLine(pen, project(p3d3), project(p3d7));
+			Point3D[] cubePoints = new Point3D[8];
+            cubePoints[0] = new Point3D(0, 0, 0);
+			cubePoints[1] = new Point3D(30, 0, 0);
+			cubePoints[2] = new Point3D(30, 0, 30);
+            cubePoints[3] = new Point3D(0, 0, 30);
+			cubePoints[4] = new Point3D(0, 30, 0);
+			cubePoints[5] = new Point3D(30, 30, 0);
+			cubePoints[6] = new Point3D(30, 30, 30);
+			cubePoints[7] = new Point3D(0, 30, 30);
+
+			Cube cube = new Cube(cubePoints);
+            cubeEngine.DrawCube(cube);
         }
     }
 }
